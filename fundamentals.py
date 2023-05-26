deb96377dbdc1484925f18a57183039e1d67157e
isahua16 <isaelhuard@gmail.com>
2023-05-25 19:14:38 -0600

    python loops

--- fundamentals.py
+++ fundamentals.py
@@ -10,11 +10,24 @@ else:
 
 if (number < 0 and is_happy == True):
     print('Negative and True')
-elif (number > 0 and is_happy == False):
+elif (number >= 0 and is_happy == False):
     print('Positive and False')
 elif (number > 100 or is_happy == True):
     print('Large or True')
 else:
     print("I don't know")
+
+my_strings = ['One', 'Two', 'Three']
+my_numbers = [10, 20, 30]
+
+for string in my_strings:
+    print(string)
+
+for number in my_numbers:
+    print('Look at this number:', number)
+
+
+
+
 
 
